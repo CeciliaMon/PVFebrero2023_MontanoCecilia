a3789130fda2325be6d9ebbf6545aa9d6e09786e
CARNEROS 53 <CARNEROS 53@DESKTOP-N6LPAND>
2023-03-02 21:32:47 -0300

    Trabajo Inicial

---
+++ PVFebrero2023_MontañoCecilia/src/main/java/ar/edu/unju/escmi/pv/PvFebrero2023MontanoCeciliaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.escmi.pv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvFebrero2023MontanoCeciliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvFebrero2023MontanoCeciliaApplication.class, args);
+	}
+
+}
---
+++ PVFebrero2023_MontañoCecilia/src/test/java/ar/edu/unju/escmi/pv/PvFebrero2023MontanoCeciliaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.escmi.pv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvFebrero2023MontanoCeciliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
